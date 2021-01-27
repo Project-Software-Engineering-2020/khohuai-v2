508d3986c472ba4fc06140ae3f8b0a5652357f25
LilyWhiteBear <59906104+LilyWhiteBear@users.noreply.github.com>
2021-01-27 22:34:34 +0700

    ongoing search and recommended

--- client/src/components/pages/Find.js
+++ client/src/components/pages/Find.js
@@ -1,7 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
 
-export default function Find() {
-  return <h1 className='find'>เอาไว้ทำหน้าหา</h1>;
+// export default function Find() {
+//   return <h1 className='find'>เอาไว้ทำหน้าหา</h1>;
   
+// }
+
+const Find = () => {
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const findAvailableLot = async () => {
+    await Axios.get("http://localhost:3001/lottery").then((lot) => {
+        setData(lot.data);
+        console.log(lot.data);
+    })
+    await setloading(true);
+  }
+
+  useEffect(async () => {
+
+    await findAvailableLot();
+    await setLoading(true);
+    
+  }, [])
+  
+  return(
+    <div>
+            {redirect ? (
+                <div>
+                    {loading ? (
+                        <div className="">
+                            <div className="container">
+                                <div className="shop">
+                                    <section className="header-shop">
+                                        <div>
+                                            <figure className="header-shop-img">
+                                                <img src="./images/store.png"></img>
+                                            </figure>
+                                        </div>
+                                        <div className="header-shop-text">
+                                            <h1>ร้านค้าสลาก</h1>
+                                            <h5>งวดประจำวันที่ 1 กุมภาพันธ์ 2563</h5>
+                                        </div>
+                                    </section>
+                                    <section className="section-search">
+                                        <div className="info-shop">
+                                            <h5>ขั้นตอนการซื้อสลากกินแบ่งรัฐบาลออนไลน์</h5>
+                                            <ol>
+                                                <li><p>เลือกสลากที่ต้องการ</p></li>
+                                                <li><p>เพิ่มลงในตะกร้า</p></li>
+                                                <li><p>ชำระเงิน</p></li>
+                                                <li><p>รอการประกาศผลรางวัล</p></li>
+                                            </ol>
+                                        </div>
+                                        <div className="box-search-lottery">
+                                            <p>ค้นหาสลาก</p>
+                                            <input type="text" className="form-control input-search-lottery" maxLength="1" />
+                                            <input type="text" className="form-control input-search-lottery" maxLength="1" />
+                                            <input type="text" className="form-control input-search-lottery" maxLength="1" />
+                                            <input type="text" className="form-control input-search-lottery" maxLength="1" />
+                                            <input type="text" className="form-control input-search-lottery" maxLength="1" />
+                                            <input type="text" className="form-control input-search-lottery" maxLength="1" />
+                                            <div className="btn-search-lottery">
+                                                <button>ล้าง</button>
+                                                <button>ค้นหา</button>
+                                            </div>
+                                        </div>
+                                    </section>
+                                    <section className="lottery-shop">
+
+                                        {data.map((item, index) => {
+                                            if (index) {
+                                                return (
+                                                    <Lottoryitem key={index} photo={item.photoURL} id={item.id}></Lottoryitem>
+                                                )
+                                            }
+
+                                        })}
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                            <div className="loader">Loading...</div>
+                        )}
+                </div>
+            ) : (
+                    <Redirect to='/login'></Redirect>
+                )}
+        </div>
+  )
+
 }
+
+export default Find;
--- client/src/components/pages/foryou.js
+++ client/src/components/pages/foryou.js
@@ -2,24 +2,54 @@ import React, { useState, useEffect } from "react"
 import Lottoryitem from "./LotteryItem"
 import './foryou.css';
 import Axios from "axios";
+import { useSelector } from 'react-redux';
 
 const Foryou = () => {
 
+    const stetus = useSelector(state => state.auth)
+    const stotus = stetus.status;
+
+    let datb = [];
     const [data, setData] = useState();
+    const [recommendedData, setRecommendedData] = useState();
     const [loading, setloading] = useState(false);
 
 
     const FetchData = async () => {
         await Axios.get("http://localhost:3001/lottery").then((lot) => {
             setData(lot.data);
+            datb = lot.data;
             console.log(lot.data);
         })
         await setloading(true);
     }
 
-    useEffect(() => {
-        FetchData();
-        console.log(data);
+    const recommended = () => {
+        if(!stotus){
+            //random
+        }
+        else{
+            //check bought
+                //...
+            //shuffle last 2 digit
+            let recommendedArray = [];
+            console.log('datb : ', datb);
+            datb.map((item, index) => {
+                if (index < 4) {
+                    let itemNum = parseInt(item.number);
+                    itemNum = (10 * (itemNum % 10)) + parseInt((itemNum % 100) / 10)
+                    recommendedArray.push(itemNum);
+                }
+            })
+            setRecommendedData(recommendedArray);
+            console.log("recommendedData : ", recommendedArray);
+        }
+    }
+
+    useEffect( async () => {
+        await FetchData();
+        await recommended();
+        //console.log(data);
     }, [])
 
     return (
