4e86ffc9cea101dacb91f208e4b720893a14b22e
guatom999 <tngs159@hotmail.com>
2021-02-04 23:33:20 +0700

    Omise

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 
 function App() {
+  
   return (
     <Provider store={store}>
       <Router>
--- client/src/components/shop/LotteryItem.js
+++ client/src/components/shop/LotteryItem.js
@@ -1,30 +1,70 @@
-import React from 'react';
-import { useDispatch } from 'react-redux' 
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
 import './LotteryItem.css'
 
-const LotteryItem = ({data}) => {
+let OmiseCard;
+
+const LotteryItem = ({ data }) => {
 
     const dispatch = useDispatch();
+        OmiseCard = window.OmiseCard;
+        OmiseCard.configure({
+            publicKey: "pkey_test_5mr6tkdorlixflztwxu",
+            currency: "thb",
+            frameLabel: "Khohuai",
+            submitLabel: "Pay Now",
+            buttomLabel: "Pay with Omise"
+        })
 
-    const addtoCart = () => {
-        dispatch({ type:"ADD_TO_CART", data: data })
+    const creditCardConfigure = () => {
+        OmiseCard.configure({
+            defaultPaymentMethod: "credit_card",
+            otherPaymentMethods: [],
+        })
+        OmiseCard.configureButton('#creditcard');
+        OmiseCard.attach();
     }
+
+    const omiseHandler = () => {
+        OmiseCard.open({
+            amount: 10000,
+            submitFormTarget: '#creditcard',
+            onCreateTokenSuccess: (token) => {
+                console.log(token)
+            },
+            onFormClosed: () => {
+            },
+        })
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        creditCardConfigure()
+        omiseHandler()
+    }
+    // const addtoCart = () => {
+    //     dispatch({ type:"ADD_TO_CART", data: data })
+    // }
     // useEffect(async ()  => {
     //     await setLottery(data)
     //     await setloading(false);
     // }, [])
 
     return (
-       
+
         <div className="lottery-item">
+            
             <figure className="lottery-item-image">
-                <a href={"/product/"+ data.id} >
-                    <img src={ data.photoURL }></img>
+                <a href={"/product/" + data.id} >
+                    <img src={data.photoURL}></img>
                 </a>
             </figure>
-            <button onClick={addtoCart} className="add-to-cart"><i className="fas fa-cart-plus"> </i> เพิ่มลงในตะกร้า</button>
+            <form>
+                <button id="creditcard"className="btn" type="button" onClick={handleClick}><i className="fas fa-cart-plus"> </i> เพิ่มลงในตะกร้า</button>
+            </form>
+            
         </div>
-       
+
     )
 }
 
