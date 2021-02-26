b4866eb1ec8c7de22a45b7eb846ea5f2138c9b9f
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-02-26 10:52:32 +0700

    router

--- client/src/App.js
+++ client/src/App.js
@@ -34,10 +34,6 @@ function App() {
 
   return (
     <Router>
-      {auth.role === "admin" ? (
-        console.log("32323")
-      ) : (
-        <div>
           <Navbar />
           <Switch>
             <Route path="/" exact={true} component={Home} />
@@ -51,9 +47,6 @@ function App() {
             <PrivateRoute path="/upload" component={UploadLottery} />
             <PrivateRoute path="/updatepassword" component={UpdatePassword} />
             <PrivateRoute path="/forgotpassword" component={ForgotPassword} />
-            {/* <Route component={Error404} /> */}
-
-            {/* <Route path='/dashbord' exact component={Overview} /> */}
             <PrivateRoute
               path="/LotteryReports"
               exact
@@ -65,8 +58,6 @@ function App() {
             <PrivateRoute path="/cart" component={Basket} />
             <PrivateRoute path="/carttest" component={CartTest} />
           </Switch>
-        </div>
-      )}
     </Router>
   );
 }
